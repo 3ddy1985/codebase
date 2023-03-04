020d755000c6d21b314963c3ff22e379c6b43b50
Nick <nick.langley@hotmail.com>
2023-03-04 04:56:19 +0000

    Fixed new error message for if a username exists in the database on the registration form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,12 @@ class ApplicationController < ActionController::Base
 
                devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :username, :email, :password, :current_password)}
           end
+
+          rescue_from ActiveRecord::RecordNotUnique, with: :show_error_message
+
+          def show_error_message(exception)
+            flash[:alert] = 'Username has already been taken'
+            redirect_to new_user_registration_path
+          end
+          
 end
